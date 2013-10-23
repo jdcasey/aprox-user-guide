f26a6c3dc6eb398ba2bf3be46e3b93dd12b280cf
John Casey <jdcasey@commonjava.org>
2013-10-23 12:34:45 -0500

    adding CC-BY-SA license notice to footers.

--- .branding/static/js/branding.js
+++ .branding/static/js/branding.js
@@ -4,9 +4,9 @@ $('.branding-header').html(
   '<div id="aprox-title"><a href="/wiki/"><span id="aprox-title-content">Aprox User Guide</span></a></div>'
 );
 
-// $('footer').prepend(
-//   '<div id="aprox-footer"><div class="license"><a href="/static/gplv3.txt"><img src="/static/images/gplv3.png"/></a><br/>Software</div><div class="license"><a href="/static/cc-by-sa.txt"><img src="/static/images/cc-by-sa.png"/></a><br/>Everything Else</div></div>'
-// );
+$('footer').prepend(
+  '<div id="aprox-footer"><div class="license"><a href="/static/cc-by-sa.txt"><img src="/static/images/cc-by-sa.png"/></a></div></div>'
+);
 
 $('.breadcrumb-sep').text('>');
 
