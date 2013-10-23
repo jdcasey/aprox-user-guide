7906f313a2df9e172898fbaaffe01d2cd3bea74d
John Casey <jdcasey@commonjava.org>
2013-10-23 00:23:22 -0500

    Adding branding js and styles, and content I would need to license this wiki cc-by-sa or gpl3

---
+++ .branding/static/js/branding.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+document.title="Aprox User's Guide"
+$('.branding-header').html(
+  '<div id="aprox-title"><a href="/wiki/"><span id="aprox-title-content">Aprox User Guide</span></a></div>'
+);
+
+// $('footer').prepend(
+//   '<div id="aprox-footer"><div class="license"><a href="/static/gplv3.txt"><img src="/static/images/gplv3.png"/></a><br/>Software</div><div class="license"><a href="/static/cc-by-sa.txt"><img src="/static/images/cc-by-sa.png"/></a><br/>Everything Else</div></div>'
+// );
+
+$('.breadcrumb-sep').text('|');
+
